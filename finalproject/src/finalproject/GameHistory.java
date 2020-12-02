dfc3a1c932ad3a3e988ccd93fc445e3be7686721
CoffeeDrinkingProgrammer <paulo.ata88@gmail.com>
2020-12-02 16:30:23 +0800

    skeleton code for GameHistory

--- finalproject/src/finalproject/GameHistory.java
+++ finalproject/src/finalproject/GameHistory.java
@@ -10,5 +10,39 @@ package finalproject;
  * @author user
  */
 public class GameHistory {
+    private Game[] history;
+    
+    //constructors
+    public GameHistory(){
+        
+    }
+    
+    
+    //getters and setters
+    public Game[] getGameHistory(){
+        return history;
+    }
+    
+    public void setGameHistory(Game[] history){
+        this.history = history.clone();
+    }
+    
+    
+    //functions
+    public boolean addGame(Game game){
+        boolean ret = false;
+        
+        return ret;
+    }
+    
+    public boolean removeGame(String id){
+        boolean ret = false;
+        
+        return ret;
+    }
+    
+    public void displayHistory(){
+        
+    }
     
 }
