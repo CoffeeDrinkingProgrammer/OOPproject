ee6132f896553d6997ad46291820e6815b6ddd2b
evad-e <cd.empinado@gmail.com>
2020-12-07 00:14:57 +0800

    added rook and bishop

--- finalproject/src/finalproject/Bishop.java
+++ finalproject/src/finalproject/Bishop.java
@@ -13,8 +13,56 @@ public class Bishop extends Piece{
     
     @Override
     public boolean canMove (Board board, Spot initialSpot, Spot finalSpot){
-        boolean ret = false;
-        
-        return ret;
+        //checks if the same color piece is on the final spot
+        Piece testPiece = finalSpot.getPiece();
+        if(testPiece!=null){ //checks if theres a piece on the final spot
+            if(testPiece.isWhite && this.isWhite){
+                return false;
+            }else if(!testPiece.isWhite && !this.isWhite){
+                return false;
+            }
+        }
+        //checks if the piece is moving properly
+        if(initialSpot.getX()==finalSpot.getX() || initialSpot.getY()==finalSpot.getY()){
+            return false;
+        }
+        if(Math.abs(finalSpot.getX()-initialSpot.getX()) != Math.abs(finalSpot.getY()-initialSpot.getY())){
+            return false;
+        }
+        //checks if there are no pieces on the way
+        int i;
+        int spaces = Math.abs(finalSpot.getX()-initialSpot.getX());
+        Spot testSpot;
+        //directions are in white perspective (not sure with this hehehe)
+        if(finalSpot.getX()>initialSpot.getX() && finalSpot.getY()>initialSpot.getY()){ //northwest
+            for(i=1;i<spaces;i++){
+                testPiece = testSpot.getPiece(initialSpot.getX()+i, initialSpot.getY()+i);
+                if(testPiece!=null){
+                    return false;
+                }
+            }
+        }else if(finalSpot.getX()>initialSpot.getX() && finalSpot.getY()<initialSpot.getY()){ //northeast
+            for(i=1;i<spaces;i++){
+                testPiece = testSpot.getPiece(initialSpot.getX()+i, initialSpot.getY()-i);
+                if(testPiece!=null){
+                    return false;
+                }
+            }
+        }else if(finalSpot.getX()<initialSpot.getX() && finalSpot.getY()<initialSpot.getY()) { //southeast
+            for (i = 1; i < spaces; i++) {
+                testPiece = testSpot.getPiece(initialSpot.getX() - i, initialSpot.getY() - i);
+                if (testPiece != null) {
+                    return false;
+                }
+            }
+        }else if(finalSpot.getX()<initialSpot.getX() && finalSpot.getY()>initialSpot.getY()){ //southwest
+            for (i = 1; i < spaces; i++) {
+                testPiece = testSpot.getPiece(initialSpot.getX() - i, initialSpot.getY() + i);
+                if (testPiece != null) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
--- finalproject/src/finalproject/Rook.java
+++ finalproject/src/finalproject/Rook.java
@@ -13,8 +13,53 @@ public class Rook extends Piece{
     
     @Override
     public boolean canMove (Board board, Spot initialSpot, Spot finalSpot){
-        boolean ret = false;
-        
-        return ret;
+
+        //checks if the same color piece is on the final spot
+        Piece testPiece = finalSpot.getPiece();
+        if(testPiece!=null){ //checks if theres a piece on the final spot
+            if(testPiece.isWhite && this.isWhite){
+                return false;
+            }else if(!testPiece.isWhite && !this.isWhite){
+                return false;
+            }
+        }
+        //checks if the piece is moving correctly
+        if(initialSpot.getX()!=finalSpot.getX() && initialSpot.getY()!=finalSpot.getY()){
+            return false;
+        }
+        //checks if there are no pieces on the way
+        int Yspaces = Math.abs(finalSpot.getY() - initialSpot.getY());
+        int Xspaces = Math.abs(finalSpot.getX() - initialSpot.getX());
+        Spot testSpot;
+        if(finalSpot.getY()>initialSpot.getY()){ //moving north (white perspective)
+            for(int i=1;i<Yspaces;i++){
+                testPiece = testSpot.getPiece(initialSpot.getX(), initialSpot.getX()+i);
+                if(testPiece !=null){
+                    return false;
+                }
+            }
+        }else if(finalSpot.getY()<initialSpot.getY()){ //moving south (white perspective)
+            for(int i=1;i<Yspaces;i++){
+                testPiece = testSpot.getPiece(initialSpot.getX(), initialSpot.getY()-i);
+                if(testPiece !=null){
+                    return false;
+                }
+            }
+        }else if(finalSpot.getX()>initialSpot.getX()){ //moving west (white perspective)
+            for(int i=1;i<Xspaces;i++){
+                testPiece = testSpot.getPiece(initialSpot.getX()+i, initialSpot.getY());
+                if(testPiece !=null){
+                    return false;
+                }
+            }
+        }else if(finalSpot.getX()<initialSpot.getX()){ //moving east (white perspective)
+            for(int i=1;i<Xspaces;i++){
+                testPiece = testSpot.getPiece(initialSpot.getX()-i, initialSpot.getY());
+                if(testPiece !=null){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
