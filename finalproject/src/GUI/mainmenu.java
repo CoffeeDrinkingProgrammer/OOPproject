6fee741373403e49d6ecc35c4c993ff70602ab95
kashmelgo <kashmelg@gmail.com>
2020-12-11 20:44:47 +0800

    unfinished GUI

---
+++ finalproject/src/GUI/mainmenu.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class mainmenu {
+    private JButton btnNewGame;
+    private JPanel panelMainMenu;
+
+    public mainmenu() {
+        btnNewGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                J
+            }
+        });
+    }
+}
