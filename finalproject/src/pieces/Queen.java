b7103a9d81e3362b709606825dd4332b417e5872
Anne Paulo <paulo.ata88@gmail.com>
2020-12-14 23:08:59 +0800

    update code for Queen

--- finalproject/src/pieces/Queen.java
+++ finalproject/src/pieces/Queen.java
@@ -4,104 +4,139 @@
  * and open the template in the editor.
  */
 package pieces;
-
+import java.util.ArrayList;
+import chess.*;
 /**
  *
  * @author user
  */
 public class Queen extends Piece{
     
+    public Queen(boolean is_white)
+    {
+        super(is_white);
+        
+    }
+    
     @Override
-    public boolean canMove (Board board, Spot initialSpot, Spot finalSpot){
+    public ArrayList<Spot> canMove(Board board, Spot initialSpot){
         
-        //checks if the same color piece is on the final spot
-        Piece testPiece = finalSpot.getPiece();
-        if(testPiece!=null){ //checks if theres a piece on the final spot
-            if(testPiece.isWhite != this.isWhite){
-                return false;
+        ArrayList<Spot> allowedMoves = new ArrayList<>();
+       
+       Spot newSpot;
+        
+       for(int i=initialSpot.getRow()+1;i<8;i++){ //moves going towards black side
+            if(board.getBox(i, initialSpot.getCol()).getPiece()==null){
+                newSpot = new Spot(i, initialSpot.getCol());
+                allowedMoves.add(newSpot);
+            }else{
+                if(board.getBox(i, initialSpot.getCol()).getPiece().isWhite() != this.isWhite()){
+                    newSpot = new Spot(i, initialSpot.getCol());
+                    allowedMoves.add(newSpot);
+                }
+                break;
+            }          
+        }
+       
+        for(int i=initialSpot.getRow()-1;i>=0;i--){ //moves going towards white side
+            if(board.getBox(i, initialSpot.getCol()).getPiece()==null){
+                newSpot = new Spot(i, initialSpot.getCol());
+                allowedMoves.add(newSpot);
+            }else{
+                if(board.getBox(i, initialSpot.getCol()).getPiece().isWhite() != this.isWhite()){
+                    newSpot = new Spot(i, initialSpot.getCol());
+                    allowedMoves.add(newSpot);
+                }
+                break;
+            }          
+        }
+        
+        for(int i=initialSpot.getCol()+1;i<8;i++){ //moves to the right (white perspective)
+            if(board.getBox(initialSpot.getRow(),i ).getPiece()==null){
+                newSpot = new Spot(initialSpot.getRow(),i);
+                allowedMoves.add(newSpot);
+            }else{
+                if(board.getBox(initialSpot.getRow(),i).getPiece().isWhite() != this.isWhite()){
+                    newSpot = new Spot(initialSpot.getRow(),i);
+                    allowedMoves.add(newSpot);
+                }
+                break;
+            }           
+        }
+        
+        for(int i=initialSpot.getCol()-1;i>=0;i--){ //moves to the left (white perspective)
+            if(board.getBox(initialSpot.getRow(),i ).getPiece()==null){
+                newSpot = new Spot(initialSpot.getRow(),i);
+                allowedMoves.add(newSpot);
+            }else{
+                if(board.getBox(initialSpot.getRow(),i).getPiece().isWhite() != this.isWhite()){
+                    newSpot = new Spot(initialSpot.getRow(),i);
+                    allowedMoves.add(newSpot);
+                }
+                break;
             }
         }
         
-        //checks if the piece is moving correctly
-        int checker = 0;
-        if(initialSpot.getX()!=finalSpot.getX() || initialSpot.getY()!=finalSpot.getY()){
-            checker++;
-        }
-        if(Math.abs(finalSpot.getX()-initialSpot.getX()) == Math.abs(finalSpot.getY()-initialSpot.getY())){
-            checker+=2;
-        }
-        if (checker == 0){
-            return false;
+        for(int i=1;i<8;i++){ //northeast (white perspective)
+            if(initialSpot.getRow()+i<8&&initialSpot.getCol()+i<8){
+                if(board.getBox(initialSpot.getRow()+i, initialSpot.getCol()+i).getPiece()==null){
+                    newSpot = new Spot(initialSpot.getRow()+i,initialSpot.getCol()+i);
+                    allowedMoves.add(newSpot);
+               }else{
+                    if(board.getBox(initialSpot.getRow()+i, initialSpot.getCol()+i).getPiece().isWhite()!=this.isWhite()){
+                        newSpot = new Spot(initialSpot.getRow()+i,initialSpot.getCol()+i);
+                        allowedMoves.add(newSpot);
+                    }
+                    break;
+                }
+           }
+       }
+        
+        for(int i=1;i<8;i++){ //southeast (white perspective)
+            if(initialSpot.getRow()-i>=0&&initialSpot.getCol()+i<8){
+                if(board.getBox(initialSpot.getRow()-i, initialSpot.getCol()+i).getPiece()==null){
+                    newSpot = new Spot(initialSpot.getRow()-i,initialSpot.getCol()+i);
+                    allowedMoves.add(newSpot);
+                }else{
+                    if(board.getBox(initialSpot.getRow()-i, initialSpot.getCol()+i).getPiece().isWhite()!=this.isWhite()){
+                        newSpot = new Spot(initialSpot.getRow()-i,initialSpot.getCol()+i);
+                        allowedMoves.add(newSpot);
+                    }
+                    break;
+                }
+           }
+       }
+       
+        for(int i=1;i<8;i++){ //northwest (white perspective)
+            if(initialSpot.getRow()+i<8&&initialSpot.getCol()-i>=0){
+                if(board.getBox(initialSpot.getRow()+i, initialSpot.getCol()-i).getPiece()==null){
+                    newSpot = new Spot(initialSpot.getRow()+i,initialSpot.getCol()-i);
+                    allowedMoves.add(newSpot);
+                }else{
+                    if(board.getBox(initialSpot.getRow()+i, initialSpot.getCol()-i).getPiece().isWhite()!=this.isWhite()){
+                        newSpot = new Spot(initialSpot.getRow()+i,initialSpot.getCol()-i);
+                        allowedMoves.add(newSpot);
+                    }
+                    break;
+                }
+           }
         }
         
-        //checks if there are no pieces on the way (white perspective == 1x1 lower leftmost spot)
-        Spot testSpot;
-        int i;
-        if (checker == 1){ //moves in one of the cardinal directions
-            int Yspaces = Math.abs(finalSpot.getY() - initialSpot.getY());
-            int Xspaces = Math.abs(finalSpot.getX() - initialSpot.getX());
-            if(finalSpot.getY()>initialSpot.getY()){ //moving north
-                for(i=1;i<Yspaces;i++){
-                    testPiece = testSpot.getPiece(initialSpot.getX(), initialSpot.getX()+i);
-                    if(testPiece != null){
-                        return false;
+        for(int i=1;i<8;i++){//southwest (white perspective)
+            if(initialSpot.getRow()-i>=0&&initialSpot.getCol()-i>=0){
+                if(board.getBox(initialSpot.getRow()-i, initialSpot.getCol()-i).getPiece()==null){
+                    newSpot = new Spot(initialSpot.getRow()-i,initialSpot.getCol()-i);
+                    allowedMoves.add(newSpot);
+                }else{
+                    if(board.getBox(initialSpot.getRow()-i, initialSpot.getCol()-i).getPiece().isWhite()!=this.isWhite()){
+                        newSpot = new Spot(initialSpot.getRow()-i,initialSpot.getCol()-i);
+                        allowedMoves.add(newSpot);
                     }
-                }
-            }else if(finalSpot.getY()<initialSpot.getY()){ //moving south
-                for(i=1;i<Yspaces;i++){
-                    testPiece = testSpot.getPiece(initialSpot.getX(), initialSpot.getY()-i);
-                    if(testPiece != null){
-                        return false;
-                    }
-                }
-            }else if(finalSpot.getX()>initialSpot.getX()){ //moving east?
-                for(i=1;i<Xspaces;i++){
-                    testPiece = testSpot.getPiece(initialSpot.getX()+i, initialSpot.getY());
-                    if(testPiece != null){
-                        return false;
-                    }
-                }
-            }else if(finalSpot.getX()<initialSpot.getX()){ //moving west?
-                for(i=1;i<Xspaces;i++){
-                    testPiece = testSpot.getPiece(initialSpot.getX()-i, initialSpot.getY());
-                    if(testPiece != null){
-                        return false;
-                    }
-                }
-            }
-        } else if (checker == 2){//moves diagonally
-            int spaces = Math.abs(finalSpot.getX()-initialSpot.getX());
-            if(finalSpot.getX()>initialSpot.getX() && finalSpot.getY()>initialSpot.getY()){ //northeast
-                for(i=1;i<spaces;i++){
-                    testPiece = testSpot.getPiece(initialSpot.getX()+i, initialSpot.getY()+i);
-                    if(testPiece!=null){
-                        return false;
-                    }
-                }
-            }else if(finalSpot.getX()>initialSpot.getX() && finalSpot.getY()<initialSpot.getY()){ //northwest
-                for(i=1;i<spaces;i++){
-                    testPiece = testSpot.getPiece(initialSpot.getX()+i, initialSpot.getY()-i);
-                    if(testPiece!=null){
-                        return false;
-                    }
-                }
-            }else if(finalSpot.getX()<initialSpot.getX() && finalSpot.getY()<initialSpot.getY()) { //southwest
-                for (i = 1; i < spaces; i++) {
-                    testPiece = testSpot.getPiece(initialSpot.getX() - i, initialSpot.getY() - i);
-                    if (testPiece != null) {
-                        return false;
-                    }
-                }
-            }else if(finalSpot.getX()<initialSpot.getX() && finalSpot.getY()>initialSpot.getY()){ //southeast
-                for (i = 1; i < spaces; i++) {
-                    testPiece = testSpot.getPiece(initialSpot.getX() - i, initialSpot.getY() + i);
-                    if (testPiece != null) {
-                        return false;
-                    }
+                    break;
                 }
             }
         }
         
-        return true;
+        return allowedMoves;
     }
 }
